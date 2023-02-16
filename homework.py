538a846111544f4d0ee80a5ffeb827292e93fa7b
Анна Саманкова <samankovaanna@yandex.ru>
2023-02-16 15:44:11 +0300

    Модуль фитнес-трекера(исправеления 2)

--- homework.py
+++ homework.py
@@ -15,8 +15,7 @@ class InfoMessage:
                     'Длительность: {duration:.3f} ч.; '
                     'Дистанция: {distance:.3f} км; '
                     'Ср. скорость: {speed:.3f} км/ч; '
-                    'Потрачено ккал: {calories:.3f}.'
-                    )
+                    'Потрачено ккал: {calories:.3f}.')
 
     def get_message(self) -> str:
         return self.MESSAGE.format(**asdict(self))
